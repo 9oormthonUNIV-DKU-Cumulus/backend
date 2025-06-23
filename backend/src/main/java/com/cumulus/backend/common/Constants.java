344516124165fcda852b76794497188ba86f5697
rudalsss <linda284@naver.com>
2025-06-24 01:38:27 +0900

    refactor: Constants 클래스 인스턴스화 방지

--- backend/src/main/java/com/cumulus/backend/common/Constants.java
+++ backend/src/main/java/com/cumulus/backend/common/Constants.java
@@ -3,6 +3,10 @@ package com.cumulus.backend.common;
 import java.util.List;
 
 public class Constants {
+    private Constants() {
+        throw new AssertionError("Cannot instantiate Constants class");
+    }
+
     // 인증필요없는 URL 리스트
     public static List<String> PERMIT_ALL_URLS = List.of(
             "/api/auth/signup",
