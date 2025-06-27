3af2d9c010d20c58e5307911e2ed794c0d5a04c3
rudalsss <linda284@naver.com>
2025-06-27 23:18:42 +0900

    entity스캐닝을 위함

--- backend/src/main/java/com/cumulus/backend/BackendApplication.java
+++ backend/src/main/java/com/cumulus/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.cumulus.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.cumulus.backend")
 public class BackendApplication {
 
 	public static void main(String[] args) {
