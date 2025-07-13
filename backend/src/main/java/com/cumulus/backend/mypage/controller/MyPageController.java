b3cd0fe1346ab4aa31defeec09dca82e4b0feabc
rudalsss <linda284@naver.com>
2025-07-13 12:06:49 +0900

    feat: 마이페이지 명세추가

--- backend/src/main/java/com/cumulus/backend/mypage/controller/MyPageController.java
+++ backend/src/main/java/com/cumulus/backend/mypage/controller/MyPageController.java
@@ -8,6 +8,8 @@ import com.cumulus.backend.common.ApplyStatus;
 import com.cumulus.backend.mypage.dto.UserInfoDto;
 import com.cumulus.backend.security.jwt.JwtUtil;
 import com.cumulus.backend.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/user/me")
 @RequiredArgsConstructor
+@Tag(name = "마이페이지")
 public class MyPageController {
 
     private final JwtUtil jwtUtil;
@@ -28,6 +31,7 @@ public class MyPageController {
     private final ActivityApplicationService activityApplicationService;
 
     @GetMapping
+    @Operation(summary = "유저기본정보 조회", description = "유저 이름, 이메일, 전공, 전화번호 등 기본정보 조회")
     public ResponseEntity<ApiResponse<?>> getMyPage( HttpServletRequest request ){
         String token = jwtUtil.resolveToken(request);
         Long userId = jwtUtil.extractUserId(token, false);
@@ -37,6 +41,7 @@ public class MyPageController {
     }
 
     @GetMapping("/activities")
+    @Operation(summary = "유저개설모임 조회", description = "해당 유저가 개설한 모임을 조회")
     public ResponseEntity<ApiResponse<?>> getHostingActivity( HttpServletRequest request ) {
         String token = jwtUtil.resolveToken(request);
         Long userId = jwtUtil.extractUserId(token, false);
@@ -46,6 +51,7 @@ public class MyPageController {
     }
 
     @GetMapping("/activity-applications/pending")
+    @Operation(summary = "유저신청 모임조회", description = "해당 유저가 신청한 모임(대기상태)을 조회")
     public ResponseEntity<ApiResponse<?>> getPendingApplication( HttpServletRequest request ){
         String token = jwtUtil.resolveToken(request);
         Long userId = jwtUtil.extractUserId(token, false);
@@ -55,6 +61,7 @@ public class MyPageController {
     }
 
     @GetMapping("/activity-applications/approve")
+    @Operation(summary = "유저신청 모임조회", description = "해당 유저가 신청한 모임(승인상태)을 조회")
     public ResponseEntity<ApiResponse<?>> getApproveApplication( HttpServletRequest request ){
         String token = jwtUtil.resolveToken(request);
         Long userId = jwtUtil.extractUserId(token, false);
