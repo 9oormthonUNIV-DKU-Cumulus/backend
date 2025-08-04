54142433f267b77d3bba2e3a2b417102367d722a
rudalsss <linda284@naver.com>
2025-08-04 09:20:29 +0900

    refactor: 재발급토큰DTO 직렬화를 위한 생성자 추가

--- backend/src/main/java/com/cumulus/backend/user/dto/ReissueRequest.java
+++ backend/src/main/java/com/cumulus/backend/user/dto/ReissueRequest.java
@@ -3,11 +3,13 @@ package com.cumulus.backend.user.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReissueRequest {
     @NotBlank(message = "재발급을 위해 refreshToken은 반드시 필요합니다.")
     private String refreshToken;
